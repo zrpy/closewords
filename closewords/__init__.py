72998038d32e8a035c8e7abd60f343e78479b6f8
zrpy <91083604+zrpy@users.noreply.github.com>
2024-12-18 22:38:10 +0900

    Create __init__.py

---
+++ closewords/__init__.py
@@ -0,0 +1,12 @@
+from rapidfuzz import fuzz
+from unidecode import unidecode
+
+def closeWords(q, candidates):
+    word = unidecode(q)
+    results = []
+    for candidate in candidates:
+        after_candidate = unidecode(candidate)
+        score = fuzz.ratio(word, after_candidate)
+        results.append({"word":candidate,"score":score})
+    results.sort(key=lambda candidates: candidates["score"], reverse=True)
+    return results
